47a35366f2cef1b706e5953367e306d9c0f82349
Mark E. DeYOung <mark.deyoung@afit.edu>
2019-03-08 15:15:06 -0500

    Fix object_detection download links

--- object_detection/download_weights.sh
+++ object_detection/download_weights.sh
@@ -1,1 +1,6 @@
-wget https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/MSRA/R-50.pkl
+# See: https://github.com/facebookresearch/Detectron/issues/1
+# See: https://github.com/facebookresearch/Detectron/blob/master/MODEL_ZOO.md#imagenet-pretrained-models
+#wget https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/MSRA/R-50.pkl
+
+wget -N https://dl.fbaipublicfiles.com/detectron/ImageNetPretrained/MSRA/R-50.pkl
+
