b94ba623cf2252c8e09df3dfff53814c72e85ffe
Mark E. DeYOung <mark.deyoung@afit.edu>
2019-03-12 12:57:16 -0400

    Fix run.sh

--- rnn_translator/pytorch/run.sh
+++ rnn_translator/pytorch/run.sh
@@ -5,14 +5,14 @@ set -e
 # Get the directory that this script is ran from
 export SOURCE_DIR=${SOURCE_DIR:="$(dirname $(readlink -f "$0"))"}
 
-DATASET_DIR='/data'
+DATASET_DIR='../data'
 RESULTS_DIR='gnmt_wmt16'
 
 SEED=${1:-"1"}
 TARGET=${2:-"21.80"}
 
 # run training
-python3 -m ${SOURCE_DIR}/multiproc train.py \
+python3 -m multiproc ${SOURCE_DIR}/train.py \
   --save ${RESULTS_DIR} \
   --dataset-dir ${DATASET_DIR} \
   --seed $SEED \
