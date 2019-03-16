2712d4b2bbd77f902a4f2c777972ee284be718d0
Mark E. DeYoung <mark.deyoung@afit.edu>
2019-03-15 21:15:11 -0400

    Updating data locations

--- translation/download_data.sh
+++ translation/download_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.en -O tensorflow/newstest2014.en
-wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.de -O tensorflow/newstest2014.de
+wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.en \
+  -O ${MLPERF_DATA_DIR}/translation/newstest2014.en
+wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.de \
+  -O ${MLPERF_DATA_DIR}/translation/newstest2014.de
 
-python3 data_download.py --raw_dir raw_data
+python3 data_download.py --raw_dir ${MLPERF_DATA_DIR}/translation/raw_data
