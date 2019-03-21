935e6e1eabc8cad6e099092f9b33223985a06359
mwnewlin <marvin.newlin@yahoo.com>
2019-03-21 14:25:02 -0400

    Singularity script updates

--- sentiment_analysis/singularity_sa_run.sh
+++ sentiment_analysis/singularity_sa_run.sh
@@ -1,27 +1,16 @@
 #!bin/bash
-#Shell script to pull docker image for Sentiment Analysis and run
+#Shell script to build singularity image from docker image for Sentiment Analysis and run
 #sentiment analysis in singularity without interaction
-cd ~
 singularity build ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg docker://cgret/sentiment_analysis:sa_1.2-gpu-cuda9.0-cudnn7
-singularity shell sentiment_analysis.simg	
-#Make directory for data	
-cd /root/.cache
-mkdir paddle
-cd paddle
-mkdir dataset
-cd dataset
-mkdir imdb
-#Make directory for sentiment analysis files
-cd /
-mkdir sentiment_analysis
-exit
+#Make directory for data
+cd Git/afit_mlperf_training/sentiment_analysis
+singularity shell ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg	-c "singularity_setup.sh"
 #Change to sentiment_analysis directory
 cd ~/Git/afit_mlperf_training/sentiment_analysis
-#Run singularity image with GPU and bind directories
+#Run singularity image with GPU (--nv) and bind directories
 singularity shell --nv \
 	--bind ${MLPERF_DATA_DIR}/sentiment_analysis:/root/.cache/paddle/dataset/imdb \
 	--bind $(pwd):/sentiment_analysis \
-	 ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg		
-#Change to directory with run script and run with seed 1
-cd sentiment_analysis/paddle
-./run_and_time.sh 1
+	 ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg \
+      -c "cd sentiment_analysis/paddle && ./run_and_time.sh 1"		
+
