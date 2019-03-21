09d450574e14dd66a5bb1a579b38ade4bbffaa8b
mwnewlin <marvin.newlin@yahoo.com>
2019-03-21 14:51:01 -0400

    script updates

--- sentiment_analysis/singularity_sa_run.sh
+++ sentiment_analysis/singularity_sa_run.sh
@@ -4,7 +4,8 @@
 singularity build ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg docker://cgret/sentiment_analysis:sa_1.2-gpu-cuda9.0-cudnn7
 #Make directory for data
 cd Git/afit_mlperf_training/sentiment_analysis
-singularity shell ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg	-c "bash singularity_setup.sh"
+singularity shell ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg	-c \
+    "cd /root/.cache && mkdir paddle && mkdir paddle/dataset && mkdir paddle/dataset/imdb"
 #Change to sentiment_analysis directory
 cd ~/Git/afit_mlperf_training/sentiment_analysis
 #Run singularity image with GPU (--nv) and bind directories
@@ -12,5 +13,5 @@ singularity shell --nv \
 	--bind ${MLPERF_DATA_DIR}/sentiment_analysis:/root/.cache/paddle/dataset/imdb \
 	--bind $(pwd):/sentiment_analysis \
 	 ${SINGULARITY_CONTAINER_PATH}/sentiment_analysis.simg \
-      -c "cd sentiment_analysis/paddle && ./run_and_time.sh 1"		
+      -c "cd /sentiment_analysis/paddle && ./run_and_time.sh 1"		
 
