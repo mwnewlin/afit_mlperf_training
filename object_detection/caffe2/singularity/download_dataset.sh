92f9995414498dedcd0b4e13056e30436b171d54
BubblePackPirate <bubblepackpirate@gmail.com>
2019-03-24 10:53:58 -0400

    Moved object detection files
    Moved and merged object detection singularity changes into the original code

--- object_detection/caffe2/singularity/download_dataset.sh
+++
@@ -1,7 +0,0 @@
-# See: https://github.com/facebookresearch/Detectron/issues/814
-#wget https://s3-us-west-2.amazonaws.com/detectron/coco/coco_annotations_minival.tgz
-wget -N https://dl.fbaipublicfiles.com/detectron/coco/coco_annotations_minival.tgz
-wget -N http://images.cocodataset.org/zips/train2014.zip
-wget -N http://images.cocodataset.org/zips/val2014.zip
-wget -N http://images.cocodataset.org/annotations/annotations_trainval2014.zip
-
--- object_detection/caffe2/singularity/download_weights.sh
+++
@@ -1,6 +0,0 @@
-# See: https://github.com/facebookresearch/Detectron/issues/1
-# See: https://github.com/facebookresearch/Detectron/blob/master/MODEL_ZOO.md#imagenet-pretrained-models
-#wget https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/MSRA/R-50.pkl
-
-wget -N https://dl.fbaipublicfiles.com/detectron/ImageNetPretrained/MSRA/R-50.pkl
-
