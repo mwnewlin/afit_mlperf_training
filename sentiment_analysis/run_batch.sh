4e11e67f2db7774e7b31d0dd0a90182e0a2bb324
Mark E. DeYoung <mark.deyoung@afit.edu>
2019-03-30 16:30:12 -0400

    Fix typo. Add some output

--- sentiment_analysis/run_batch.sh
+++ sentiment_analysis/run_batch.sh
@@ -3,12 +3,15 @@ cd ${HOME}/git/afit_mlperf_training/sentiment_analysis
 
 conda activate paddlepaddle-gpu
 
-for in in {1..100}
+for i in {1..100}
 do
 	# Run native
+	echo "sentiment_analysis: native, run ${i}"
 	bash paddle/run_and_time.sh &> "$(hostname).$(date --date "now" +"%Y-%m-%d-%H-%M").native.log"
 
 	# Run singularity
+	# Note: need sudo on DL/ML boxes due to permission configuration on NAS.
+	echo "sentiment_analysis: singularity, run ${i}"
 	singularity exec \
 		--nv \
 		--bind $(pwd):/benchmark \
