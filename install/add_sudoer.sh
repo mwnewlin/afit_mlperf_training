0f6822eb66897f031aa528a83cf0e5e84fe3501b
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2019-03-31 13:56:03 -0400

    Update add_sudoer.sh

--- install/add_sudoer.sh
+++ install/add_sudoer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Error: must be run as root"
+if [ "$EUID" -ne 0 ]; then
+  echo "Error: must be run as root"
   exit 1
 fi
 
-if [[ ! ${1} ]]
-  then "Error: You must provide a username for an existing user."
+if [[ ! ${1} ]]; then
+  echo "Error: You must provide a username for an existing user."
   exit 1
 fi
 
@@ -14,8 +14,11 @@ fi
 GROUP=$(getent group sudo | cut -d':' -f1)
 GROUP=${GROUP:-$(getent group wheel | cut -d':' -f1)}
   
-usermod -aG ${GROUP} ${1}
+# If the usermod succeeds then 
+# create a sudoers.d file for the user.
+if usermod -aG ${GROUP} ${1}; then
+  FILE="/etc/sudoers.d/${1}"
+  echo "${1} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
+fi
 
-FILE="/etc/sudoers.d/${1}"
-echo "${1} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
 
