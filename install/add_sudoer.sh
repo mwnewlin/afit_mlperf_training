bb14408161fa225fcf5beac978073643475789f7
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2019-03-31 13:11:39 -0400

    Update add_sudoer.sh

--- install/add_sudoer.sh
+++ install/add_sudoer.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-FILE="/etc/sudoers.d/${USER}"
-echo "${USER} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
+
+if [ "$EUID" -ne 0 ]
+  then echo "Error: must be run as root"
+  exit 1
+fi
+
+if [[ ! ${1} ]]
+  then "Error: You must provide a username for an existing user."
+  exit 1
+fi
+  
+usermod -aG sudo ${1}
+
+FILE="/etc/sudoers.d/${1}"
+echo "${1} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
 
