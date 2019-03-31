8a64fd4ed36fbd56dea7e9adde921454a3f18a38
Mark E. DeYoung <mark.deyoung@afit.edu>
2019-03-31 15:49:22 -0400

    Cleanup.

--- translation/tensorflow/singularity/download_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd tensorflow
-wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.en -O newstest2014.en
-wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.de -O newstest2014.de
-
-python3 data_download.py --raw_dir raw_data
