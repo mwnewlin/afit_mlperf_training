52dd5345517e1e6d565db5227fc8a5c0aeb9968e
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2019-03-31 13:47:43 -0400

    Update add_sudoer.sh

--- install/add_sudoer.sh
+++ install/add_sudoer.sh
@@ -9,8 +9,12 @@ if [[ ! ${1} ]]
   then "Error: You must provide a username for an existing user."
   exit 1
 fi
+
+# use sudo or wheel
+GROUP=$(getent group sudo | cut -d':' -f1)
+GROUP=${GROUP:-$(getent group wheel | cut -d':' -f1)}
   
-usermod -aG sudo ${1}
+usermod -aG GROUP ${1}
 
 FILE="/etc/sudoers.d/${1}"
 echo "${1} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
