e729f7797226843bca11cf3c8f31f0470f2fd514
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2019-04-02 13:47:18 -0400

    ${OUTPUT_DIR} for each run
    Create a unique ${OUTPUT_DIR} for each run to prevent race conditions.

--- recommendation/pytorch/run_and_time.sh
+++ recommendation/pytorch/run_and_time.sh
@@ -10,6 +10,11 @@ BASEDIR=$(dirname -- "$0")
 
 DATA_DIR="${MLPERF_DATA_DIR}/recommendation"
 
+# Set OUTPUT_DIR default to include PBS_JOBID (if it is defined)
+#  or a UUID if PBS_JOBID is not defined.
+#  This specifies a unique output directory for each run.
+OUTPUT_DIR=${2:-"${DATA_DIR}/${PBS_JOBID:-$(uuidgen -t)}"}
+
 PYTHONPATH=${BASEDIR}:${PYTHONPATH}
 
 # start timing
@@ -25,14 +30,14 @@ if unzip -u ${DATA_DIR}/ml-20m.zip -d ${DATA_DIR}
 then
     echo "Start processing ${DATA_DIR}/ml-20m/ratings.csv"
     t0=$(date +%s)
-	python ${BASEDIR}/convert.py ${DATA_DIR}/ml-20m/ratings.csv ml-20m --negatives 999
+	python ${BASEDIR}/convert.py ${DATA_DIR}/ml-20m/ratings.csv ${OUTPUT_DIR} --negatives 999
     t1=$(date +%s)
 	delta=$(( $t1 - $t0 ))
     echo "Finish processing ${DATA_DIR}/ml-20m/ratings.csv in $delta seconds"
 
     echo "Start training"
     t0=$(date +%s)
-	python ${BASEDIR}/ncf.py ml-20m -l 0.0005 -b 2048 --layers 256 256 128 64 -f 64 \
+	python ${BASEDIR}/ncf.py ${OUTPUT_DIR} -l 0.0005 -b 2048 --layers 256 256 128 64 -f 64 \
 		--seed $seed --threshold $THRESHOLD --processes 10
     t1=$(date +%s)
 	delta=$(( $t1 - $t0 ))
