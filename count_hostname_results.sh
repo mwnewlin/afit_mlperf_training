1aff4f94d6720a9997e4b406b9f23b8456e7a599
Mark E. DeYoung <mark.deyoung@afit.edu>
2019-04-08 16:15:32 -0400

    Add count_hostname_results.sh

---
+++ count_hostname_results.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BENCHMARKS=( sentimement_analysis 
+	rnn_translator
+	recommendation
+	translation object_detection )
+
+for BENCHMARK in "${BENCHMARKS[@]}"
+do
+	NATIVE_COUNT=$(grep RESULT ${BENCHMARK}/$(hostname).*.native.log 2>/dev/null | wc -l )
+	SINGULARITY_COUNT=$(grep RESULT ${BENCHMARK}/$(hostname).*.singularity.log 2>/dev/null | wc -l )
+	echo "${BENCHMARK}: ${SERVER}: ${NATIVE_COUNT}, ${SINGULARITY_COUNT}"
+	done
+done
+
+
